6665c69a0d4a1ec5293c6deae6e4b52c501d8433
Erangr90 <erangrady3.0@gmail.com>
2025-07-01 11:39:40 +0300

    frontend production

--- src/server.ts
+++ src/server.ts
@@ -21,14 +21,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(
-//   cors({
-//     // origin: "http://localhost:5000",
-//     credentials: true,
-//   }),
-// );
+app.use(
+  cors({
+    origin: "https://frontend-question.onrender.com",
+    credentials: true,
+  }),
+);
 
-app.use(cors());
+// app.use(cors());
 
 app.use(cookieParser());
 
